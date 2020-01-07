ce899c3490c17870372e70d67d9ea88374c2f1d5
mccalldan <drm8266@gmail.com>
2020-01-06 22:36:24 -0500

    adding click and press event listeners

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function () {
 
-  var x = 23;
 
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-      publicTest: function(b) {
-        return add(b);
-      }
-  }
 
 })();
 
-
+// UI CONTROLLER
 var uIController = (function() {
 
 
 
 })();
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-var z = budgetCtrl.publicTest(5);
+  var ctrlAddItem = function() {
+    // 1. Get field input data
 
-  return {
-      budgetTest: function() {
-        console.log(z);
-      }
+    // 2. Add the item to the budget CONTROLLER
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budge
+
+    console.log('It works');
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+
+    if(event.keyCode === 13 || event.which === 13) {
+    ctrlAddItem();
+    }
+
+  });
 
 })(budgetController, uIController);
