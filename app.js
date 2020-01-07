ddb47598d132e4885f1f928dc5224057d892fd2b
mccalldan <drm8266@gmail.com>
2020-01-07 09:19:56 -0500

    init function

--- app.js
+++ app.js
@@ -9,16 +9,51 @@ var budgetController = (function () {
 // UI CONTROLLER
 var uIController = (function() {
 
+  var dOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
 
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(dOMstrings.inputType).value, // will be inc or exp
+        description: document.querySelector(dOMstrings.inputDescription).value,
+        value: document.querySelector(dOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return dOMstrings;
+    }
+  };
 
 })();
 
 // GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function(budgetCtrl, uICtrl) {
+
+  setUpEventListeners = function() {
+    var dOM = uICtrl.getDOMstrings();
+
+    document.querySelector(dOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+      if(event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+      }
+      });
+  };
+
+
 
   var ctrlAddItem = function() {
     // 1. Get field input data
-
+    var input = uICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget CONTROLLER
 
     // 3. Add the item to the UI
@@ -27,17 +62,19 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 5. Display the budge
 
-    console.log('It works');
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-  document.addEventListener('keypress', function(event) {
+  return {
+    init: function() {
+      console.log('App started!');
+      setUpEventListeners();
 
-    if(event.keyCode === 13 || event.which === 13) {
-    ctrlAddItem();
     }
+  };
 
-  });
 
 })(budgetController, uIController);
+
+
+controller.init();
